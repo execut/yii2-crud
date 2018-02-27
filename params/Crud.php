ea1fa353add0111961df410825cef3d77429014b
eXeCUT <sexecut@gmail.com>
2018-02-27 12:29:39 +0300

    added alert in grid config

--- params/Crud.php
+++ params/Crud.php
@@ -43,7 +43,7 @@ class Crud extends BaseObject
                                         'export' => new UnsetArrayValue(),
                                         'refresh' => new UnsetArrayValue(),
                                     ],
-                                    'layout' => '{toolbar}<br><br>{items}',
+                                    'layout' => '{alertBlock}{toolbar}<br><br>{items}',
                                     'filterPosition' => '123',
                                 ],
                             ],
